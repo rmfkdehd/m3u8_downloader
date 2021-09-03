228c624ac6b82cf96f6368b88ad54fa0f81530db
rlahflflf <zxc011117@gmail.com>
2021-09-03 22:25:42 +0900

    Update m3u8_downloader.py

--- m3u8_downloader.py
+++ m3u8_downloader.py
@@ -23,7 +23,7 @@ headers = {
 # m3u8链接批量输入文件(必须是utf-8编码)
 m3u8InputFilePath = "/content/m3u8_input.txt"
 # 设置视频保存路径
-saveRootDirPath = "/content"
+saveRootDirPath = "/content/"
 # 下载出错的m3u8保存文件
 errorM3u8InfoDirPath = "/content/error.txt"
 # m3u8文件、key文件下载尝试次数，ts流默认无限次尝试下载，直到成功
